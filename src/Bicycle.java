c1f7ebaf9ecc07ffb590b2e9455843325615b7e2
agorohov <aleksandr.gorohov@gmail.com>
2024-04-16 11:12:11 +0300

    доделал ДЗ

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -3,4 +3,8 @@ public class Bicycle extends WheeledTransport {
     public Bicycle(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
+
+    public Bicycle(String modelName) {
+        super(modelName, 2);
+    }
 }
--- src/Car.java
+++ src/Car.java
@@ -3,4 +3,8 @@ public class Car extends MotorTransport {
     public Car(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
+
+    public Car(String modelName) {
+        super(modelName, 4);
+    }
 }
